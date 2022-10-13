0e70e86cfefeffdc9a7927c47b2db2d1cbfa4422
EightFiveZeroEight <business.8508@gmail.com>
2022-10-13 15:33:21 -0400

    Declare win condition true and false conditions

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 let winningNumber = Math.floor((Math.random()*100)+1);
-console.log(winningNumber);
+// Above is the variable that determins the number the players is trying to guess.
+
+let playerGuess = undefined;
+// Above is the player input of their guess
+    // Should only be recorded after a button click
+
+let winCondition = false;
+
+// This needs to be wrapped inside an event listener to the result of player input
+    if (playerGuess === winningNumber) {
+        winCondition == true;
+    } else if (playerGuess != winningNumber) {
+        winCondition == false;
+        tryCounter ++;
+    }
+
+// Below: if the player wins reset try counter and assign new number
+if (winCondition == true) {
+    tryCounter = 0;
+    winningNumber = Math.floor((Math.random()*100)+1);
+}
